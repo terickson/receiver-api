1d22ed1257a262b593f869331238912e10e408e1
Todd Erickson <todd1981@gmail.com>
2018-12-27 21:43:12 -0600

    making it so put acts like patch

--- routes/receiver.py
+++ routes/receiver.py
@@ -30,13 +30,13 @@ def show():
 def update():
     current_app.logger.info("update receiver")
     receiverInfo = request.json
-    if current_app.rx.on != receiverInfo['on']:
+    if 'on' in receiverInfo and current_app.rx.on != receiverInfo['on']:
         current_app.rx.on = receiverInfo['on']
     if current_app.rx.on:
-        if current_app.rx.volume != receiverInfo['volume']:
+        if 'volume' in receiverInfo and current_app.rx.volume != receiverInfo['volume']:
             current_app.rx.volume = receiverInfo['volume']
-        if current_app.rx.input != receiverInfo['input']:
+        if 'input' in receiverInfo and current_app.rx.input != receiverInfo['input']:
             current_app.rx.input = receiverInfo['input']
-        if current_app.rx.mute != receiverInfo['mute']:
+        if 'mute' in receiverInfo and current_app.rx.mute != receiverInfo['mute']:
             current_app.rx.mute = receiverInfo['mute']
     return Response(response=json.dumps(receiverInfo), mimetype='application/json')
